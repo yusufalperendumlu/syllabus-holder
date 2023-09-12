8974cf5d767ebc217ed0a328e6f13724e946f3f7
Yusuf Alperen <yusufalperendumlu@gmail.com>
2023-09-12 21:30:42 +0300

    Update lesson.reducer.ts

--- src/app/stateManagement/reducers/lesson.reducer.ts
+++ src/app/stateManagement/reducers/lesson.reducer.ts
@@ -9,7 +9,21 @@ export interface IState {
 
 //Initial State
 export const initialLessonState: IState = {
-    data: Array<Lesson>()
+    data: getLessonsFromLocalStorage() || [] // Önce yerel depodan dersleri al veya boş bir dizi oluştur
+}
+
+// Function to get lessons from local storage
+function getLessonsFromLocalStorage(): Lesson[] | null {
+    const storedData = localStorage.getItem('lessons');
+    if (storedData) {
+        return JSON.parse(storedData);
+    }
+    return null;
+}
+
+// Function to save lessons to local storage
+function saveLessonsToLocalStorage(lessons: Lesson[]) {
+    localStorage.setItem('lessons', JSON.stringify(lessons));
 }
 
 //Reducer
@@ -18,15 +32,19 @@ export const lessonReducer = createReducer(initialLessonState,
         const newState: IState = {
             data: [...state.data, lesson]
         }
+        saveLessonsToLocalStorage(newState.data); // Eklenen dersleri yerel depoya kaydet
         return newState;
     }),
     on(removeLesson, (state, lesson) => {
-        const deleteLesson = state.data.find(l => l.id == lesson.id);
-        const deleteLessonIndexNo = state.data.indexOf(deleteLesson);
-        const newState: IState = {
-            data: [...state.data]
-        };
-        newState.data.splice(deleteLessonIndexNo, 1);
-        return newState;
+        const deleteLessonIndexNo = state.data.findIndex(l => l.id === lesson.id);
+        if (deleteLessonIndexNo !== -1) {
+            const newState: IState = {
+                data: [...state.data]
+            };
+            newState.data.splice(deleteLessonIndexNo, 1);
+            saveLessonsToLocalStorage(newState.data); // Silinen dersleri yerel depodan kaldır
+            return newState;
+        }
+        return state;
     })
 );
